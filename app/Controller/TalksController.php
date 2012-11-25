3776ead794a47cc4d9298c61ede22e34f32a56f0
James Fuller <jblotus@gmail.com>
2012-11-25 17:22:23 -0500

    added talks controller

---
+++ app/Controller/TalksController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Talks Controller
+ *
+ */
+class TalksController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
---
+++ app/Test/Case/Controller/TalksControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('TalksController', 'Controller');
+
+/**
+ * TalksController Test Case
+ *
+ */
+class TalksControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.talk'
+	);
+
+}
