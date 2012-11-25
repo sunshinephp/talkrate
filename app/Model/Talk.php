f0081c7a818884eb4cfcc50fcb6a3e4ea4088b2b
James Fuller <jblotus@gmail.com>
2012-11-25 16:14:27 -0500

    added user and talk models

---
+++ app/Model/Talk.php
@@ -0,0 +1,13 @@
+<?php
+
+App::uses('Model', 'Model');
+
+/**
+ * Talk model
+ *
+ * Contains logic related to talks
+ *
+ * @package       app.Model
+ */
+class Talk extends Model {
+}
---
+++ app/Model/User.php
@@ -0,0 +1,13 @@
+<?php
+
+App::uses('Model', 'Model');
+
+/**
+ * User model
+ *
+ * Contains logic related to users
+ *
+ * @package       app.Model
+ */
+class User extends Model {
+}
